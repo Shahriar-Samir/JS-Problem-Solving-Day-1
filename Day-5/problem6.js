754d1a6222c87d518f25c03633999388df1f3917
Shahriar-Samir <shabusiness035@gmail.com>
2024-10-11 11:29:00 +0600

    add: day-6

---
+++ Day-5/problem6.js
@@ -0,0 +1,5 @@
+//  some of array
+
+const sum= (arr)=> arr.reduce((acc,current)=> acc+current)
+
+console.log(sum([1,2,3,4,5]))
---
+++ Day-6/problem1.js
@@ -0,0 +1,6 @@
+//  reverse an string 
+
+const reverseString = (str)=> str.split('').reverse().join('')
+
+
+console.log(reverseString('Samir'))
