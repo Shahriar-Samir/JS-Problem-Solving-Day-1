05784a42b970cb2dbb54af5bf109ec5579567eab
Shahriar-Samir <shabusiness035@gmail.com>
2024-10-18 14:33:13 +0600

    add: day-12

--- Day-11/problem3.js
+++ Day-11/problem3.js
@@ -1,1 +1,5 @@
 // capitalize
+
+const capitalize = (str)=> str.split(' ').map(item=> item.replace(item[0],item[0].toUpperCase())).join(' ')
+
+console.log(capitalize('this is a very good thing'))
---
+++ Day-12/problem1.js
@@ -0,0 +1,5 @@
+//  sum of array 
+
+const sumOfArray = (arr)=> arr.reduce((acc,c)=> acc+c)
+
+console.log(sumOfArray([1,2,3,4,5]))
