6d185659c17cae8e8d0f7f0dc5d8d6289485ba9e
Shahriar-Samir <shabusiness035@gmail.com>
2024-11-19 18:50:08 +0600

    add: day-29

--- Day-29/problem1.js
+++ Day-29/problem1.js
@@ -9,3 +9,73 @@ const palindrome = (str) => {
 };
 
 console.log(palindrome("MoM"));
+
+const palindrome1 = (str) => {
+  let reversedString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedString += str[i];
+  }
+  return reversedString === str;
+};
+
+console.log(palindrome2("DaD"));
+
+const palindrome2 = (str) => {
+  let reversedStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return str === reversedStr;
+};
+
+const palindrome3 = (str) => {
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return str === reversedStr;
+};
+
+const palindrome4 = (str) => {
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr === str;
+};
+
+const palindrome5 = (str) => {
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+
+  return reversedStr === str;
+};
+
+const palindrome6 = (str) => {
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 1; i--) {
+    reversedStr += str[i];
+  }
+
+  return reversedStr === str;
+};
+
+const palindrome7 = (str) => {
+  const reversedStr = "";
+
+  for (let i = str.length; i >= 1; i--) {
+    reversedStr += str[i];
+  }
+
+  return reversedStr === str;
+};
+
+const removeDups3 = (str) => {
+  return str
+    .split("")
+    .filter((item, index) => index === str.index(item))
+    .join("");
+};
