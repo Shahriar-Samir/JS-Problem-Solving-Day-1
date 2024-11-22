b61eacf3f239eee12b6c43cdfec073b065eb1e46
Shahriar-Samir <shabusiness035@gmail.com>
2024-11-22 07:49:35 +0600

    add: day-31

---
+++ Day-32/problem1.js
@@ -0,0 +1,9 @@
+const palidrome = (str) => {
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr === str;
+};
+
+console.log(palidrome(""));
